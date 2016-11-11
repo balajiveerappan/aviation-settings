74ce54bbb317bec0e67f62ce45f40a49703ffd3f
Bansal <amit.bansal@capgemini.com>
2016-11-11 11:11:41 +0530

    first commit

---
+++ src/main/java/com/aviation/poc/settings/AviationSettingsApplication.java
@@ -0,0 +1,12 @@
+package com.aviation.poc.settings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviationSettingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviationSettingsApplication.class, args);
+	}
+}
